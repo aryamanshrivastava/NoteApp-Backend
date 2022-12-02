1745576389590b3455975cb9d25103615fa9663d
Aryaman <aryamanvirus08@gmail.com>
2022-12-02 17:03:49 +0530

    Backend

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const serverless = require('serverless-http');
+//const serverless = require('serverless-http');
 const app = express();
 
 const mongoose = require('mongoose');
 const Note = require('./models/Note');
 const router = require('./routes/Note');
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({extended:false}));
-// app.use(bodyParser.json());
-app.use('/.netlify/functions/api',router);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+//app.use('/.netlify/functions/api',router);
 const mongoDbPath = "mongodb+srv://aryaman:aryaman@cluster0.ojnrfla.mongodb.net/notesdb";
 mongoose.connect(mongoDbPath).then(function()
 {
@@ -25,8 +25,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,function(){
     console.log("Server started at PORT:" + PORT);
 });
-module.exports = app;
-module.exports.handler = serverless(app)
+// module.exports = app;
+// module.exports.handler = serverless(app)
 // // "start": "node src/server.js",//package.json scripts
 // "test": "echo \"Error: no test specified\" && exit 1",
 //     "dev": "nodemon src/server.js",
