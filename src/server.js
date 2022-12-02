5c394b1aa382a4fcd1a53d1fad2e2c788627d2a1
Aryaman <aryamanvirus08@gmail.com>
2022-12-02 16:59:29 +0530

    Backend

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 
 const mongoose = require('mongoose');
 const Note = require('./models/Note');
+const router = require('./routes/Note');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+app.use('/.netlify/functions/api',router);
 const mongoDbPath = "mongodb+srv://aryaman:aryaman@cluster0.ojnrfla.mongodb.net/notesdb";
 mongoose.connect(mongoDbPath).then(function()
 {
     app.get("/",function(req,res){
-        const response = {message: "API Works!"};
+        const response = {statuscode:res.statusCode,  message: "API Works!"};
         res.json(response);
     });
 
@@ -23,3 +25,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,function(){
     console.log("Server started at PORT:" + PORT);
 });
+module.exports = app;
+module.exports.handler = serverless(app)
+// // "start": "node src/server.js",//package.json scripts
+// "test": "echo \"Error: no test specified\" && exit 1",
+//     "dev": "nodemon src/server.js",
